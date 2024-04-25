f1dd7b5c9542a3b55358e8adb2d9b0697212e059
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-04-25 16:37:16 -0500

    switched everything over to new repo, replaced api keys

---
+++ src/api/apiConstants.js
@@ -0,0 +1,8 @@
+// apiConstants.js
+export const APP_ID = import.meta.env.VITE_APP_ID;
+export const APP_KEY = import.meta.env.VITE_APP_KEY;
+
+// console.log('APP_ID:', process.env.REACT_APP_APP_ID);
+// console.log('APP_KEY:', process.env.REACT_APP_APP_KEY);
+// export const APP_ID = "b8ba7f6a";
+// export const APP_KEY = "92ce080756aca9f7ef28aa6ca4e2a668";
---
+++ src/views/error/ErrorPage.jsx
@@ -0,0 +1,12 @@
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1 className="flex justify-center text-4xl mt-24 text-danger font-medium">
+        404 Page Not Found
+      </h1>
+    </div>
+  )
+}
+
+export default ErrorPage
