3c186b16594d25afecd47f0f7798e92a9cb7763e
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-04-26 09:22:47 -0500

    Added more descriptive text within slider

--- src/data.js
+++ src/data.js
@@ -9,25 +9,25 @@ const sliderData = [
   {
     id: 1,
     title: "Baked Chicken Breast",
-    text: "A handful of simple ingredients typify the fresh, vibrant flavors of Greek cooking.",
+    text: "Juicy and tender chicken breast perfectly baked to a golden finish. Seasoned with a harmonious blend of herbs and spices, this dish offers a delightful burst of flavors that bring out the natural goodness of chicken.",
     image: slider_images[0],
   },
   {
     id: 2,
     title: "Homemade Chicken Tikka Masala",
-    text: "A handful of simple ingredients typify the fresh, vibrant flavors of Greek cooking.",
+    text: "A rich and creamy Chicken Tikka Masala that brings the authentic taste of Indian cuisine to your plate. Marinated chicken pieces are cooked in a spicy and aromatic tomato-based sauce, paired with a side of fluffy basmati rice.",
     image: slider_images[1],
   },
   {
     id: 3,
-    title: "Meat Burger with salad cheese tomato and ketchup",
-    text: "A handful of simple ingredients typify the fresh, vibrant flavors of Greek cooking.",
+    title: "Deluxe Cheeseburger With Fries",
+    text: "A mouthwatering deluxe cheeseburger featuring a thick, juicy beef patty, melted cheese, crisp lettuce, fresh tomatoes, and a dollop of our signature sauce, all encased in a soft, toasted bun. Served with a side of golden, crispy fries.",
     image: slider_images[2],
   },
   {
     id: 4,
-    title: "Raspberry Tartlet dessert with almong flakes",
-    text: "A handful of simple ingredients typify the fresh, vibrant flavors of Greek cooking.",
+    title: "Raspberry Tart Dessert With Chocolate",
+    text: "An exquisite raspberry tart layered with rich, luscious chocolate ganache and adorned with fresh, plump raspberries on top. The perfect blend of tartness and sweetness makes this dessert a decadent treat for any occasion.",
     image: slider_images[3],
   },
 ];
